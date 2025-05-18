7b26bc9fe87e07cb068a64281dcbcea94a1fc0a7
Aman Sabir <amansabir@Amans-MBP.modem>
2025-05-18 17:40:18 +1000

    Cleanup refactored code and add console input

--- src/main/java/org/logparser/com/Main.java
+++ src/main/java/org/logparser/com/Main.java
@@ -1,13 +1,16 @@
 package org.logparser.com;
 
-import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.logging.Logger;
+import org.logparser.com.service.LogFileService;
+import org.logparser.com.service.LogLinesParsingService;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -17,58 +20,13 @@ public class Main {
     private static final Logger logger = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
-        try {
-            print(extractIPsAsStream());
-            print(extractUrlsAsStream());
-            extractUniqueIPs();
-            extractUniqueUrls();
-            getTopNElements(extractIPsAsStream(), 3);
-            getTopNElements(extractUrlsAsStream(), 3);
+        Scanner scanner = new Scanner(System.in);
 
-
-
-        } catch (IOException e) {
-            logger.info("Error reading file" + e.getMessage());
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static List<String> getTopNElements(Stream<String> stringStream, int n) throws IOException {
-        List<String> topNElements = stringStream.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
-                .entrySet().stream()
-                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
-                .limit(n)
-                .map(Map.Entry::getKey)
-                .collect(Collectors.toList());
-        logger.info("Top N elements" +topNElements);
-
-        return null;
-    }
-
-
-    private static Stream<String> extractUniqueUrls() throws IOException {
-        logger.info("Unique URLS");
-        logger.info(extractUrlsAsStream().collect(Collectors.toSet()).toString());
-        return null;
-    }
-    private static Stream<String> extractUniqueIPs() throws IOException {
-        logger.info("Unique IPs");
-        logger.info(extractIPsAsStream().collect(Collectors.toSet()).toString());
-        return null;
-    }
-
-    private static void print(Stream<String> stringStream) throws IOException {
-        logger.info("List");
-        logger.info(stringStream.collect(Collectors.toList()).toString());
-    }
-
-    private static Stream<String> extractUrlsAsStream() throws IOException {
-        Path logPath = Path.of("/Users/amansabir/Desktop/projects/personal-workspace/IPTracker/src/main/resources/programming-task-example-data.log");
-        return Files.lines(logPath)
-                .map(line -> line.split(" ")[6]);
-    }
-    private static Stream<String> extractIPsAsStream() throws IOException {
-        return Files.lines(Path.of("/Users/amansabir/Desktop/projects/personal-workspace/IPTracker/src/main/resources/programming-task-example-data.log"))
-                .map(line -> line.split(" ")[0]);
+        System.out.print("Enter the log file path: ");
+        String filePath = scanner.nextLine();
+        LogFileService logFileService = new LogFileService(Path.of(filePath), new LogLinesParsingService());
+        System.out.println(logFileService.extractTopThreeActiveIPs());
+        System.out.println(logFileService.extractTopThreeVisitedURLs());
+        System.out.println(logFileService.extractUniqueIPs());
     }
 }
